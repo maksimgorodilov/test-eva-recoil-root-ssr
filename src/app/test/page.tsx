f01283e390436242513e1d79613628ee83a945df
Maksim Gorodilov <matim@mbp-van-maksim.home>
2024-02-26 16:38:24 +0100

    feat: add EvaRecoilRoot

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import {EvaRecoilRoot} from "@springtree/eva-sdk-react-recoil";
+import {EvaWrapper} from "@/components/eva-wrapper";
+
+export default function Page() {
+    return (
+        <EvaWrapper>
+            <div>Hello World</div>
+        </EvaWrapper>
+    );
+}
---
+++ src/components/eva-wrapper/eva-wrapper.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { ReactNode } from 'react';
+import {EvaRecoilRoot} from "@springtree/eva-sdk-react-recoil";
+export const EvaWrapper = ({children}: {children: ReactNode}) => (
+    <EvaRecoilRoot endpointUrl="https://api.rituals.test.eva-online.cloud">
+        {children}
+    </EvaRecoilRoot>
+)
