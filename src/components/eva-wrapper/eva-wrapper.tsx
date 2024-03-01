b3ab75e9698e6abe6548ec7f69af25e9e57bc4d6
Maksim Gorodilov <matim@mbp-van-maksim.home>
2024-03-01 12:14:04 +0100

    chore: bump deps

--- src/components/eva-wrapper/eva-wrapper.tsx
+++ src/components/eva-wrapper/eva-wrapper.tsx
@@ -3,9 +3,11 @@
 import { ReactNode } from 'react';
 import {EvaRecoilRoot} from "@springtree/eva-sdk-react-recoil";
 export const EvaWrapper = ({children}: {children: ReactNode}) => (
-    <EvaRecoilRoot endpointUrl="https://api.rituals.test.eva-online.cloud">
-        <div>
-            {children}
-        </div>
-    </EvaRecoilRoot>
+    <div>
+        <EvaRecoilRoot endpointUrl="https://api.rituals.test.eva-online.cloud">
+            <div>
+                {children}
+            </div>
+        </EvaRecoilRoot>
+    </div>
 )
